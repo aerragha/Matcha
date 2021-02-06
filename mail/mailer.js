95c7f15f16e0d7ea6bd9f0b81f6d4aa966ec9277
Ayoub Erraghay <aerragha@e2r1p3.1337.ma>
2021-02-06 12:26:44 +0100

    add mail

---
+++ mail/mailer.js
@@ -0,0 +1,21 @@
+const nodemailer = require('nodemailer');
+
+const  transport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'matcha1337@gmail.com',
+        pass: '2019y2019'
+    }
+});
+
+module.exports = {
+    sendEmail(to, subject, html) {
+        return new Promise((resolve, reject) => {
+            var from = 'matcha1337@gmail.com';
+            transport.sendMail({ from, subject, to, html}, (err, info) => {
+                if(err) reject(err);
+                resolve(info);
+            });
+        });
+    }
+}
